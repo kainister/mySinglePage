f7cae1ad1795d5bc3a7b090a568ec70f2046bd8d
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-02-06 18:12:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status === 0)
+            {
+                var allText = rawFile.responseText;
+                displayFileContent(allText);
+            }
+        }
+    };
+    rawFile.send(null);
+}
+function displayFileContent(alltext) {
+    var content = document.querySelector('#content');
+    content.innerHTML = alltext;
+}
+window.onload =function () {
+    document.querySelector('#myForm').onsubmit = function () {
+        var username = document.querySelector('.username').value;
+        sessionStorage.setItem('username', username);
+        console.log('azert');
+    };
+
+    document.querySelector('.submit').onclick = function () {
+        readTextFile('accueil.html');
+    };
+};
