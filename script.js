5692acc90c5020112ee5ffd043411536de1b78f7
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-03-06 00:42:01 +0100

    added edit username

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function readTextFile(file)
             document.querySelector('#content').innerHTML =
                 this.responseText;
         }
-        document.querySelector('#username').innerHTML = username;
+        if (file === 'profile.html') {
+            var name = document.querySelector('#username');
+            name.setAttribute('value', username);
+            console.log('test');
+            document.querySelector('#edit_btn').onclick = function () {
+                console.log('ca marche');
+                sessionStorage.clear('username');
+                sessionStorage.setItem('username', name.value);
+            };
+
+        } else {
+            document.querySelector('#username').innerHTML = username;
+        }
         document.querySelector('.profile').innerHTML = "<h1>"+username+"</h1>";
         document.querySelector('.home').onclick = function () {
             readTextFile('home.html');
