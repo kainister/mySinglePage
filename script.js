1a1d035b25a3ab0fcb7aa65106380729d16346d3
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-03-06 00:12:49 +0100

    clean code

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
 function readTextFile(file)
 {
+    var username = sessionStorage.getItem('username');
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('#content').innerHTML =
                 this.responseText;
         }
+        document.querySelector('#username').innerHTML = username;
+        document.querySelector('.profile').innerHTML = "<h1>"+username+"</h1>";
+        document.querySelector('.home').onclick = function () {
+            readTextFile('home.html');
+        };
+        document.querySelector('.profile').onclick = function () {
+            readTextFile('profile.html');
+        };
     };
     xhttp.open("GET", file, true);
     xhttp.send();
 }
 
-function displayProfile() {
-    setTimeout(function () {
-        document.querySelector('.profile').onclick = function () {
-            readTextFile('profile.html');
-            setTimeout(function () {
-                document.querySelector('#username').innerHTML = username;
-            }, 100);
-        }
-    }, 100);
-    setTimeout(function () {
-        displayHome();
-    }, 100);
-}
-
-function displayHome() {
-    setTimeout(function () {
-        document.querySelector('.home').onclick = function () {
-            readTextFile('home.html');
-        }
-    }, 100);
-    setTimeout(function () {
-        displayProfile();
-    }, 100);
-}
-var username = sessionStorage.getItem('username');
 window.onload =function () {
     if (sessionStorage.getItem('username') !== null) {
-        var username = sessionStorage.getItem('username');
         readTextFile('home.html');
-        displayProfile();
-        displayHome();
 
     }
     document.querySelector('#myForm').onsubmit = function () {
@@ -53,8 +34,6 @@ window.onload =function () {
         }
         sessionStorage.setItem('username', userName);
         readTextFile('home.html');
-        displayProfile();
-        displayHome();
         return false;
     };
 };
